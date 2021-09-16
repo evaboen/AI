73b51707c4567544e4b24e2ad38fc879034fc923
Erlend Vaboen <erlevab@stud.ntnu.no>
2021-09-16 16:19:02 +0200

    Start implement A_Star

--- A_Star.py
+++ A_Star.py
@@ -1,5 +1,54 @@
+import Map
+
+global_map = Map.Map_Obj()
+
+class Node:
+    def __init__(self, x, y, goal, parent=None):
+        self.pos = [x,y]
+        self.parent = parent
+        if parent is not None:
+            self.g = parent.get_g() + 1
+        else:
+            self.g = 0
+        self.h = abs(self.pos[0] - goal[0]) + abs(self.pos[1] - goal[1])
+
+    def get_g(self):
+        return self.g
+
+    def get_children(self):
+        children = []
+        if self.parent is None:
+            for x in range(-1, 2, 2):
+                for y in range(-1, 2, 2):
+                    print(x, y)
+                    c_pos = [self.pos[0] + x, self.pos[1] + y]
+                    if global_map.get_cell_value(c_pos) == 1:
+                        children.append(c_pos)
+        else:
+            for x in range(-1, 2, 2):
+                for y in range(-1, 2, 2):
+                    c_pos = [self.pos[0] + x, self.pos[1] + y]
+                    if c_pos == self.parent.pos and global_map.get_cell_value(c_pos) == 1:
+                        children.append(c_pos)
+        return children
+
+
+n = Node(27, 13, global_map.goal_pos)
+print(n.get_children())
+
+
+
+
 
 class A_Star:
+    def __init__(self):
+        self.map = Map.map_obj()
+        self.current_pos = self.map.start_pos
 
-    def start(self):
-        print("hell0")
+
+    def h(self):
+
+        return
+
+    def f(self):
+        return g()
